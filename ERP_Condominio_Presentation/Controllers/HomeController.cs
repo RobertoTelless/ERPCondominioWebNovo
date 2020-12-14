f52bff156d72ddc3840c0b0d4f9b82202c4ab6b5
Roberto Telles <robertotelles00@gmail.com>
2020-12-14 18:25:20 -0300

    Upload 14/12/2020 2

--- ERP_Condominio_Presentation/Controllers/HomeController.cs
+++ ERP_Condominio_Presentation/Controllers/HomeController.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ApplicationServices.Interfaces;
+using EntitiesServices.Model;
+using System.Globalization;
+using EntitiesServices.Work_Classes;
+using ERP_Condominio_Presentation.App_Start;
+using AutoMapper;
+using ERP_Condominio.ViewModels;
+using System.IO;
+using System.Collections;
 
 namespace ERP_Condominio_Presentation.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUsuarioAppService baseApp;
+        private readonly INotificacaoAppService notiApp;
+        private readonly ILogAppService logApp;
+        private String msg;
+        private Exception exception;
+        USUARIO objeto = new USUARIO();
+        USUARIO objetoAntes = new USUARIO();
+        List<USUARIO> listaMaster = new List<USUARIO>();
+
+        public HomeController(IUsuarioAppService baseApps, ILogAppService logApps, INotificacaoAppService notApps)
+        {
+            baseApp = baseApps;
+            logApp = logApps;
+            notiApp = notApps;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return RedirectToAction("Login", "ControleAcesso");
         }
 
         public ActionResult About()
